3d5decc330273b6e998efe48165a7d196d2a91b1
Tserke <d.Tserkezidis@gmail.com>
2021-02-16 17:19:04 +0200

    building core classes

---
+++ crmApp/crmApp/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrmApp
+{
+    class Customer
+    {
+    }
+}
---
+++ crmApp/crmApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrmApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Ui ui = new Ui();
+
+            Product myProduct = ui.CreateProduct();
+            if(myProduct != null)
+            {
+                myProduct.Print();
+
+                decimal total = myProduct.TotalCost;
+                Console.WriteLine(total);
+            }
+           
+        }
+    }
+}
